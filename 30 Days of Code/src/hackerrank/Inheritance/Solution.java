378c546b0ed990654bece3159f53855f58b38bb3
Lenovo <Lenovo@VICTORROMAN-PC>
2021-07-26 17:25:19 -0500

    Commit reto day 12

--- 30 Days of Code/src/hackerrank/Inheritance/Solution.java
+++ 30 Days of Code/src/hackerrank/Inheritance/Solution.java
@@ -3,18 +3,29 @@
  */
 package hackerrank.Inheritance;
 
+import java.util.Scanner;
+
 /**
  * @author Lenovo
  *
  */
 public class Solution {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		String firstName = scan.next();
+		String lastName = scan.next();
+		int id = scan.nextInt();
+		int numScores = scan.nextInt();
+		int[] testScores = new int[numScores];
+		for(int i = 0; i < numScores; i++){
+			testScores[i] = scan.nextInt();
+		}
+		scan.close();
+		
+		Student s = new Student(firstName, lastName, id, testScores);
+		s.printPerson();
+		System.out.println("Grade: " + s.calculate());
 	}
 
 }
