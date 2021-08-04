50fccb11d6a959008160b5a5479dee40f8b2945b
Victor Roman Garrido <ing.victor.roman@gmail.com>
2021-08-03 20:16:40 -0500

    commit reto 21

---
+++ 30 Days of Code/src/hackerrank/generic/Generics.java
@@ -0,0 +1,40 @@
+/**
+ * Creado por: Victor Roman Garrido
+ * Fecha:3/08/2021 7:59:08 p. m.
+ */
+package hackerrank.generic;
+
+import java.util.Scanner;
+
+/**
+ * @author Victor Roman Garrido
+ *
+ */
+public class Generics {
+	
+	public static void main(String args[]){
+		
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+        
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray(intArray);
+        stringPrinter.printArray(stringArray);
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    } 
+
+}
---
+++ 30 Days of Code/src/hackerrank/generic/Printer.java
@@ -0,0 +1,21 @@
+/**
+ * Creado por: Victor Roman Garrido
+ * Fecha:3/08/2021 8:00:17 p. m.
+ */
+package hackerrank.generic;
+
+/**
+ * @author Victor Roman Garrido
+ *
+ */
+public class Printer <T> {
+
+	public void printArray(T[] Array) {
+		// TODO Auto-generated method stub
+		
+		for (T i : Array) {
+			System.out.println(i);
+		}
+	}
+	
+}
